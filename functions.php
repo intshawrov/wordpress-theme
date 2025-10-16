ac1a26d57bae2dc933d0a5c8f01c9349f341edde
Shawrov <intshawrov@gmail.com>
2025-10-16 18:12:21 +0600

    Initial commit

--- functions.php
+++ functions.php
@@ -4,3 +4,5 @@ add_theme_support('menus');
 add_theme_support('widgets');
 add_theme_support('title-tag');
 add_theme_support('post-thumbnail');
+
+
--- index.php
+++ index.php
@@ -4,8 +4,11 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Document</title>
-      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+
 </head>
+
+      <?php wp_head();  ?>
 <body>
    
 
@@ -21,5 +24,7 @@
             </div>
       </div>
       
+
+      <?php wp_footer(); ?>
 </body>
 </html>
