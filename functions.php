a551deff0d61ceee1e48be081b1e56205fe753c5
Shawrov <intshawrov@gmail.com>
2025-10-16 18:21:43 +0600

    Initial commit

--- functions.php
+++ functions.php
@@ -3,6 +3,6 @@
 add_theme_support('menus');
 add_theme_support('widgets');
 add_theme_support('title-tag');
-add_theme_support('post-thumbnail');
+add_theme_support('post-thumbnails');
 
 
---
+++ single.php
@@ -0,0 +1,41 @@
+<!-- <?php get_header(); ?>
+
+<?php
+if (have_posts()) :
+    while (have_posts()) : the_post(); ?>
+
+        <h1><?php the_title(); ?></h1>
+        <?php
+        if (has_post_thumbnail()) {
+            the_post_thumbnail('full');
+        }
+        ?>
+        <div><?php the_content(); ?></div>
+
+    <?php endwhile;
+else :
+    echo '<p>No posts found</p>';
+endif;
+?>
+
+<?php get_footer(); ?> -->
+
+
+<?php get_header(); ?>
+
+<?php 
+if(have_posts()):
+      while( have_posts()) : the_post(); ?>
+      <h1><?php the_title(); ?></h1>
+      <?php 
+      if(has_post_thumbnail()){
+            the_post_thumbnail('full');
+      }
+      ?>
+      <div><?php the_content(); ?></div>
+      <?php endwhile; 
+      else: 
+            echo '<p> No posts found</p> ';
+      endif;
+      ?>
+      <?php get_footer(); ?>
