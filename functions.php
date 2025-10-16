f0f4fb0938743714ff8aff8f87eece4dab562420
Shawrov <intshawrov@gmail.com>
2025-10-16 19:36:46 +0600

    Inatial commit

--- functions.php
+++ functions.php
@@ -5,4 +5,12 @@ add_theme_support('widgets');
 add_theme_support('title-tag');
 add_theme_support('post-thumbnails');
 
-
+function mir_enqueue_styles() {
+    wp_enqueue_style(
+        'google-fonts',
+        'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap',
+        array(),
+        null
+    );
+}
+add_action('wp_enqueue_scripts', 'mir_enqueue_styles');
