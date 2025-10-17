a4d693e4eb1ef1a3a334fe4c000a45258e512c5d
Shawrov <intshawrov@gmail.com>
2025-10-17 19:08:41 +0600

    Initial commit

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ add_theme_support('menus');
 add_theme_support('widgets');
 add_theme_support('title-tag');
 add_theme_support('post-thumbnails');
+add_theme_support('header');
 
 function mir_enqueue_styles() {
     wp_enqueue_style(
